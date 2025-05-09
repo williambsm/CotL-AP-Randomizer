c09e2f7825aee83524aaf0486d2099623cfea33a
William Ste-Marie <williambsm@hotmail.fr>
2025-05-09 12:42:42 -0400

    Add first locations and items

--- CotL-AP-Randomizer/Archipelago/Items.cs
+++ CotL-AP-Randomizer/Archipelago/Items.cs
@@ -3,5 +3,11 @@ namespace CotL_AP_Randomizer.Archipelago
     public enum Items
     {
         Unset = -1,
+        ItemFollowerAmdusias,
+        ItemFollowerValefar,
+        ItemFollowerBarbatos,
+        ItemFollowerLeshy,
+        ItemLeshyProgression,
+        ItemLeshyHeart,
     }
 } 
--- CotL-AP-Randomizer/Archipelago/Locations.cs
+++ CotL-AP-Randomizer/Archipelago/Locations.cs
@@ -3,5 +3,14 @@ namespace CotL_AP_Randomizer.Archipelago
     public enum Locations
     {
         Unset = -1,
+        DefeatBossAmdusiasProgression,
+        DefeatBossAmdusiasFollower,
+        DefeatBossValefarProfession,
+        DefeatBossValefarFollower,
+        DefeatBossBarbatosProgression,
+        DefeatBossBarbatosFollower,
+        DefeatBishopLeshyProgression,
+        DefeatBishopLeshyFollower,
+        DefeatBishopLeshyHeart
     }
 } 
